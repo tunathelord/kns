645dc320bdcdb9cb6eee11cfe65d973824be1d06
tuna8126-netizen <trantrungnam2006@gmail.com>
2026-05-15 02:34:00 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('ptg-v1').then((cache) => cache.addAll(['index.html', 'manifest.json']))
+  );
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
